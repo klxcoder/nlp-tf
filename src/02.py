4f80291d5cf3b4fc2824abcfb02e865ae5abc34a
klxcoder <klxcoder@gmail.com>
2025-04-17 22:42:26 +0700

    texts_to_sequences

--- src/02.py
+++ src/02.py
@@ -11,3 +11,5 @@ tokenizer = Tokenizer(num_words = 100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 print(word_index) # {'i': 1, 'love': 2, 'my': 3, 'dog': 4, 'cat': 5}
+sequences = tokenizer.texts_to_sequences(sentences)
+print(sequences) # [[1, 2, 3, 4], [1, 2, 3, 5]]

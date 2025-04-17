0e2c78c08d99bd53d25f4a2d259995e86cfa6ca6
klxcoder <klxcoder@gmail.com>
2025-04-17 22:49:42 +0700

    dog!

--- src/04.py
+++ src/04.py
@@ -11,3 +11,21 @@ tokenizer = Tokenizer(num_words = 100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 print(word_index) # {'i': 1, 'love': 2, 'my': 3, 'dog': 4, 'cat': 5}
+
+sentences = [
+    'I love my dog',
+    'I love my cat',
+    'You love my dog!'
+]
+
+tokenizer = Tokenizer(num_words = 100)
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+print(word_index) # {'love': 1, 'my': 2, 'i': 3, 'dog': 4, 'cat': 5, 'you': 6}
+
+"""
+Note:
+    + high frequency words will be appeared first
+    + words will be converted to lowercase
+    + "dog!" and "dog" will be treated the same
+"""

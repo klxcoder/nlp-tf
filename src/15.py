d68a9af735fa1a92948684c400d066126394b032
klxcoder <klxcoder@gmail.com>
2025-04-18 15:53:34 +0700

    split sentences and labels into training and testing

--- src/15.py
+++ src/15.py
@@ -16,17 +16,8 @@ with open('./dataset/Sarcasm_Headlines_Dataset.json', 'r') as f:
         labels.append(item['is_sarcastic'])
         urls.append(item['article_link'])
         count += 1
-        if count==2:
+        if count==10:
             break
-
-# print(sentences)
-# ["former versace store clerk sues over secret 'black code' for minority shoppers", "the 'roseanne' revival catches up to our thorny political mood, for better and worse"]
-
-# print(labels)
-# [0, 0]
-
-# print(urls)
-# ['https://www.huffingtonpost.com/entry/versace-black-code_us_5861fbefe4b0de3a08f600d5', 'https://www.huffingtonpost.com/entry/roseanne-revival-review_us_5ab3a497e4b054d118e04365']
 
 tokenizer = Tokenizer(oov_token="<OOV>")
 tokenizer.fit_on_texts(sentences)
@@ -34,4 +25,36 @@ word_index = tokenizer.word_index
 sequences = tokenizer.texts_to_sequences(sentences)
 padded = pad_sequences(sequences)
 print(padded[0]) # [ 0  0  3  4  5  6  7  8  9 10 11  2 12 13]
-print(padded.shape) # (2, 14)
+print(padded.shape) # (10, 14)
+
+training_size = 8
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+print(json.dumps(training_sentences, indent=2))
+"""
+[
+  "former versace store clerk sues over secret 'black code' for minority shoppers",
+  "the 'roseanne' revival catches up to our thorny political mood, for better and worse",
+  "mom starting to fear son's web series closest thing she will have to grandchild",
+  "boehner just wants wife to listen, not come up with alternative debt-reduction ideas",
+  "j.k. rowling wishes snape happy birthday in the most magical way",
+  "advancing the world's women",
+  "the fascinating case for eating lab-grown meat",
+  "this ceo will send your kids to school, if you work for his company"
+]
+"""
+
+print(json.dumps(testing_sentences, indent=2))
+"""
+[
+  "top snake handler leaves sinking huckabee campaign",
+  "friday's morning email: inside trump's presser for the ages"
+]
+"""
+
+print(training_labels) # [0, 0, 1, 1, 0, 0, 0, 0]
+
+print(testing_labels) # [1, 0]

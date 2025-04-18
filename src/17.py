347acf3ef5d52702517a2a44c557112f2004a21b
klxcoder <klxcoder@gmail.com>
2025-04-18 17:17:16 +0700

    use sarcasm data to train and test

--- src/17.py
+++ src/17.py
@@ -4,15 +4,17 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
 
 vocab_size = 10000
 embedding_dim = 16
+max_length = 100
 trunc_type='post'
 padding_type='post'
 oov_tok = "<OOV>"
 training_size = 20000
 
-count = 0
+# count = 0
 
 sentences = []
 labels = []
@@ -24,59 +26,31 @@ with open('./dataset/Sarcasm_Headlines_Dataset.json', 'r') as f:
         sentences.append(item['headline'])
         labels.append(item['is_sarcastic'])
         urls.append(item['article_link'])
-        count += 1
-        if count==10:
-            break
+        # count += 1
+        # if count==10:
+        #     break
 
-training_size = 8
+# training_size = 8
+
 training_sentences = sentences[0:training_size]
 testing_sentences = sentences[training_size:]
 training_labels = labels[0:training_size]
 testing_labels = labels[training_size:]
-
-# print(json.dumps(training_sentences, indent=2))
-"""
-[
-  "former versace store clerk sues over secret 'black code' for minority shoppers",
-  "the 'roseanne' revival catches up to our thorny political mood, for better and worse",
-  "mom starting to fear son's web series closest thing she will have to grandchild",
-  "boehner just wants wife to listen, not come up with alternative debt-reduction ideas",
-  "j.k. rowling wishes snape happy birthday in the most magical way",
-  "advancing the world's women",
-  "the fascinating case for eating lab-grown meat",
-  "this ceo will send your kids to school, if you work for his company"
-]
-"""
-
-# print(json.dumps(testing_sentences, indent=2))
-"""
-[
-  "top snake handler leaves sinking huckabee campaign",
-  "friday's morning email: inside trump's presser for the ages"
-]
-"""
-
-# print(training_labels) # [0, 0, 1, 1, 0, 0, 0, 0]
-
-# print(testing_labels) # [1, 0]
 
 tokenizer = Tokenizer(oov_token="<OOV>")
 tokenizer.fit_on_texts(training_sentences)
 word_index = tokenizer.word_index
 
 training_sequences = tokenizer.texts_to_sequences(training_sentences)
+# Filter out tokens >= num_words
+training_sequences = [[token for token in seq if token < vocab_size] for seq in training_sequences]
 training_padded = pad_sequences(training_sequences)
 
 testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_sequences = [[token for token in seq if token < vocab_size] for seq in testing_sequences]
 testing_padded = pad_sequences(testing_sequences)
 
-# print(testing_sequences) # [[1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 3, 4, 1]]
-
-# print(testing_padded)
-"""
-[[0 0 1 1 1 1 1 1 1]
- [1 1 1 1 1 1 3 4 1]]
-"""
+assert training_padded.max() < vocab_size
 
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim),
@@ -96,3 +70,23 @@ history = model.fit(
     validation_data=(np.array(testing_padded), np.array(testing_labels)),
     verbose=2,
 )
+
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_' + string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.legend([string, 'val_' + string])
+    plt.show()
+
+# plot_graphs(history, "accuracy")
+# plot_graphs(history, "loss")
+
+sentence = [
+    "granny starting to fear spiders in the garden might be real",
+    "game of thrones season finale showing this sunday night",
+]
+sequences = tokenizer.texts_to_sequences(sentence)
+sequences = [[token for token in seq if token < vocab_size] for seq in sequences]
+padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+print(model.predict(padded))

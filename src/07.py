5420ba548a1cf0fa5e0c12b2765a88a200bcc97d
klxcoder <klxcoder@gmail.com>
2025-04-18 10:57:56 +0700

    oov_token keep the original length

--- src/07.py
+++ src/07.py
@@ -9,15 +9,15 @@ sentences = [
     'Do you think my dog is amazing?',
 ]
 
-tokenizer = Tokenizer(num_words = 100)
+tokenizer = Tokenizer(num_words = 100, oov_token="<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 print(word_index)
-# {'my': 1, 'love': 2, 'dog': 3, 'i': 4, 'you': 5, 'cat': 6, 'do': 7, 'think': 8, 'is': 9, 'amazing': 10}
+# {'<OOV>': 1, 'my': 2, 'love': 3, 'dog': 4, 'i': 5, 'you': 6, 'cat': 7, 'do': 8, 'think': 9, 'is': 10, 'amazing': 11}
 
 sequences = tokenizer.texts_to_sequences(sentences)
 print(sequences)
-# [[4, 2, 1, 3], [4, 2, 1, 6], [5, 2, 1, 3], [7, 5, 8, 1, 3, 9, 10]]
+# [[5, 3, 2, 4], [5, 3, 2, 7], [6, 3, 2, 4], [8, 6, 9, 2, 4, 10, 11]]
 
 test_data = [
     'i really love my dog',
@@ -26,4 +26,4 @@ test_data = [
 
 test_sequences = tokenizer.texts_to_sequences(test_data)
 print(test_sequences)
-# [[4, 2, 1, 3], [1, 3, 1]]
+# [[5, 1, 3, 2, 4], [2, 4, 1, 2, 1]]
